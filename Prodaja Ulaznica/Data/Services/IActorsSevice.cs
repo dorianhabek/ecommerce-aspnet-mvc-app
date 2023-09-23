65b2f37713588efe9a2266f9f91b3d3f8fe9e0a4
dorianhabek <dorianhabek@gmail.com>
2023-09-23 10:12:41 +0200

    dodavanje Iactors service

---
+++ Prodaja Ulaznica/Data/Services/IActorsSevice.cs
@@ -0,0 +1,14 @@
+﻿using Prodaja_Ulaznica.Models;
+
+namespace Prodaja_Ulaznica.Data.Services
+{
+    public interface IActorsSevice
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+
+        void Delete(int id);
+    }
+}
